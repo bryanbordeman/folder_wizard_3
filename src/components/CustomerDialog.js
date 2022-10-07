c698f624aea2f81050edb288cfdc25391bf60024
Bryan Bordeman <bryanbordeman@Bryans-MBP.home>
2022-10-07 04:41:49 -0400

    added delete button to CustomerDialog

--- src/components/CustomerDialog.js
+++ src/components/CustomerDialog.js
@@ -8,10 +8,14 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Box from '@mui/material/Box';
 import Transition from './DialogTransistion'
+import AddressPicker from './AddressPicker';
+import CloseIcon from '@mui/icons-material/Close';
+import { Stack, IconButton } from '@mui/material';
 
 export default function CustomerDialog(props) {
     const { customerData , open, setOpen, token, handleOpenSnackbar, setCustomers, customers } = props;
     const [ customer, setCustomer ] = useState({});
+    const [ values, setValues ] = useState('');
 
     useEffect(() => {
         setCustomer(customerData);
@@ -49,6 +53,22 @@ export default function CustomerDialog(props) {
         });
     };
 
+    const deleteCompany = (id) => {
+        CompanyServices.deleteCompany(id, token)
+        .then(response => {
+            handleOpenSnackbar('error', 'Company was deleted')
+            const updatedCustomers = customers.filter(el => (
+                el.id !== customer.id
+            ))
+            setCustomers(updatedCustomers)
+            setOpen(false);
+        })
+        .catch(e => {
+            console.log(e);
+            handleOpenSnackbar('error', 'Something Went Wrong!! Please try again.')
+        });
+    };
+
     return (
         <Box>
             <Dialog 
@@ -57,13 +77,29 @@ export default function CustomerDialog(props) {
                 open={open} 
                 onClose={handleClose}
             >
-                <DialogTitle>Edit Customer</DialogTitle>
+                <DialogTitle>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>
+                            Edit Customer
+                        </div>
+                        <div>
+                        <IconButton 
+                            edge="end" 
+                            aria-label="close"
+                            onClick={handleClose}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                        </div> 
+                    </div>
+                </DialogTitle>
                 <DialogContent>
+                    <Stack spacing={2}>
                     <TextField
                         autoFocus
                         margin="dense"
                         id="name"
-                        label="Name"
+                        label="Company Name"
                         defaultValue={customerData.name}
                         type="text"
                         fullWidth
@@ -72,8 +108,53 @@ export default function CustomerDialog(props) {
                             e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g,"")
                         }}
                     />
+                    <AddressPicker
+                        token={token} 
+                        handleOpenSnackbar={handleOpenSnackbar}
+                        values={values}
+                        setValues={setValues}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="phone"
+                        label="Phone"
+                        type="text"
+                        fullWidth
+                        // onChange={handleUpdateCustomer}
+                        onInput = {(e) =>{
+                            e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g,"")
+                        }}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="fax"
+                        label="Fax"
+                        type="text"
+                        fullWidth
+                        // onChange={handleUpdateCustomer}
+                        onInput = {(e) =>{
+                            e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g,"")
+                        }}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="website"
+                        label="Website"
+                        type="text"
+                        fullWidth
+                        // onChange={handleUpdateCustomer}
+                        onInput = {(e) =>{
+                            e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g,"")
+                        }}
+                    />
+                    <Button variant='contained' color='secondary'>Add Contact</Button>
+                    </Stack>
                 </DialogContent>
                 <DialogActions>
+                    <Button variant='outlined' color='error' onClick={() => {deleteCompany(customerData.id)}}>Delete</Button>
                     <Button variant='outlined' onClick={handleClose}>Cancel</Button>
                     <Button variant='contained' onClick={handleUpdate}>Update</Button>
                 </DialogActions>
